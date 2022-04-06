25cda61e1260d3c61083e727ea5e11305b31f6ec
TTIT\bakp <pawel.bak@ttpsc.pl>
2022-04-06 18:30:25 +0200

    Initialized project

---
+++ Projekt/src/main/java/com/example/projekt/HelloController.java
@@ -0,0 +1,37 @@
+package com.example.projekt;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
+
+public class HelloController {
+    @FXML
+    private ProgressBar progressBar_server1;
+    @FXML
+    private Label label_server1;
+    @FXML
+    private ProgressBar progressBar_server2;
+    @FXML
+    private Label label_server2;
+    @FXML
+    private ProgressBar progressBar_server3;
+    @FXML
+    private Label label_server3;
+    @FXML
+    private ProgressBar progressBar_server4;
+    @FXML
+    private Label label_server4;
+    @FXML
+    private ProgressBar progressBar_server5;
+    @FXML
+    private Label label_server5;
+
+    @FXML
+    private ListView usersList;
+
+    @FXML
+    protected void onGenerateButtonClick() {
+
+    }
+}
